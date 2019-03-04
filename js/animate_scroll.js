add0e67c4397f22494f20a1c47797aab61ae5c0c
wilfriedaugeard <augeardw@gmail.com>
2019-03-04 11:17:16 +0100

    Add files via upload

---
+++ js/animate_scroll.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $(".scroll_animate").on("click", function(event){
+    
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        
+    });
+
+})
